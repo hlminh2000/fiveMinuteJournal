6d644089a09553b1f2adc82ce251e520d4695168
Minh <hlminh2000@gmail.com>
2017-03-05 19:12:57 -0500

    some stuff

--- components/Scenes/ListPage.js
+++ components/Scenes/ListPage.js
@@ -39,35 +39,58 @@ export default class ListPage extends Component {
               selectedBackgroundColor={'#a9a9a9'} />
           </View>
         </Image>
-        <ScrollView>
-          <ListItem>
-            <Icon name="ios-sunny-outline"></Icon>
-            <View>
-              <View><H3 style={{marginLeft:10, paddingBottom:5, color:'darkgrey'}}>What I am grateful for...</H3></View>
-              <View><Text style={{marginLeft:10}}>1) What I am grateful for...</Text></View>
-              <View><Text style={{marginLeft:10}}>2) What I am grateful for...</Text></View>
-              <View><Text style={{marginLeft:10}}>3) What I am grateful for...</Text></View>
-            </View>
-          </ListItem>
-          <ListItem>
-            <Icon name="ios-sunny-outline"></Icon>
-            <View>
-              <View><H3 style={{marginLeft:10, paddingBottom:5, color:'darkgrey'}}>What would make today great</H3></View>
-              <View><Text style={{marginLeft:10}}>What I am grateful for...</Text></View>
-            </View>
-          </ListItem>
-          <ListItem>
-            <Icon name="ios-sunny-outline"></Icon>
-            <View><Text style={{marginLeft:10}}>"Daily affirmations"</Text></View>
-          </ListItem>
-          <ListItem>
-            <Icon name="ios-sunny-outline"></Icon>
-            <View><Text style={{marginLeft:10}}>"3 amazing things that happened today"</Text></View>
-          </ListItem>
-          <ListItem>
-            <Icon name="ios-sunny-outline"></Icon>
-            <View><Text style={{marginLeft:10}}>"How could you have made today better?"</Text></View>
-          </ListItem>
+        <ScrollView style={{position:'absolute', height:1000}}>
+          <View style={{height:320}}></View>
+          <View style={{height:1000, backgroundColor:'white'}}>
+
+            <List>
+              <ListItem>
+                <Icon name="ios-sunny-outline"/>
+                <View style={{marginLeft:15}}>
+                  <View><Text style={{fontSize:18, color:'darkgrey'}}>What I am grateful for?</Text></View>
+                  <Text>1) The color of my beautiful eyes</Text>
+                  <Text>2) My perfect wave hair</Text>
+                  <Text>3) How awesome I am</Text>
+                </View>
+              </ListItem>
+
+              <ListItem>
+                <Icon name="ios-sunny-outline"/>
+                <View style={{marginLeft:15}}>
+                  <View><Text style={{fontSize:18, color:'darkgrey'}}>What would make today great?</Text></View>
+                  <Text>1) Finish my project</Text>
+                  <Text>2) Submit my application</Text>
+                  <Text>3) Go to the gym</Text>
+                </View>
+              </ListItem>
+
+              <ListItem>
+                <Icon name="ios-sunny-outline"/>
+                <View style={{marginLeft:15}}>
+                  <View><Text style={{fontSize:18, color:'darkgrey'}}>Daily Affirmation. I am...</Text></View>
+                  <Text>The strongest version of myself</Text>
+                </View>
+              </ListItem>
+
+              <ListItem>
+                <Icon name="ios-moon"/>
+                <View style={{marginLeft:15}}>
+                  <View><Text style={{fontSize:18, color:'darkgrey'}}>3 Amazing things that happened today</Text></View>
+                  <Text>1) I was a mentor at a major hackathon</Text>
+                  <Text>2) I am building my first mobile app</Text>
+                  <Text>3) I was almost a judge</Text>
+                </View>
+              </ListItem>
+
+              <ListItem>
+                <Icon name="ios-moon"/>
+                <View style={{marginLeft:15}}>
+                  <View><Text style={{fontSize:18, color:'darkgrey'}}>How Could I have made today better</Text></View>
+                  <Text>Go to the gym...</Text>
+                </View>
+              </ListItem>
+            </List>
+          </View>
         </ScrollView>
       </View>
     );
