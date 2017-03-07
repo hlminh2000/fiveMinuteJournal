2d9df6472b2aa37189b11013b3723578b12ee4f3
Minh <hlminh2000@gmail.com>
2017-03-07 08:11:51 -0500

    parallax

--- components/Scenes/ListPage.js
+++ components/Scenes/ListPage.js
@@ -20,30 +20,85 @@ import {
   H3
 } from 'native-base';
 import CalendarPicker   from "react-native-calendar-picker";
-
+import Calendar from 'react-native-calendar';
+import ParallaxView from 'react-native-parallax-view';
 
 export default class ListPage extends Component {
 
-  render() {
-    return (
-      <View>
-        <Image
-          source={{uri: 'http://digioh.com/blog/wp-content/uploads/2016/08/teaser.jpg'}}
-          style={{height: 320}}>
-          <View style={{backgroundColor:'#000000'+'26', height:320}}>
-            <CalendarPicker
-              textStyle     =   {{color:'white'}}
-              selectedDate  =   {new Date()}
-              onDateChange  =   {()=>{}}
-              screenWidth   =   {Dimensions.get('window').width}
-              selectedBackgroundColor={'#a9a9a9'} />
-          </View>
-        </Image>
-        <ScrollView style={{position:'absolute', height:1000}}>
-          <View style={{height:320}}></View>
-          <View style={{height:1000, backgroundColor:'white'}}>
+  constructor(props){
+    super(props);
+    this.state = {
+      date  : new Date(),
+    }
+    this.onDateChange = this.onDateChange.bind(this);
+  }
 
+  onDateChange(date){
+    this.setState({
+      date  : date,
+    });
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <ParallaxView
+        backgroundSource = {{uri: 'http://digioh.com/blog/wp-content/uploads/2016/08/teaser.jpg'}}
+        windowHeight={305}
+        header={(
+          <Calendar
+            scrollEnabled={false}
+            showControls={true}
+            customStyle={{
+              calendarContainer:{
+                backgroundColor : 'rgba(0, 0, 0, 0)'
+              },
+              day: {
+                color: 'white'
+              },
+              calendarHeading: {
+                borderColor: 'rgba(0, 0, 0, 0)'
+              },
+              dayHeading: {
+                color: 'white'
+              },
+              controlButtonText: {
+                color: 'white'
+              },
+              title: {
+                color: 'white'
+              },
+              weekendDayText: {
+                color: 'darkgrey'
+              },
+              weekendHeading: {
+                color: 'darkgrey'
+              }
+            }}/>
+        )}>
+
+        {/* <ScrollView
+          style={{position:'absolute', height:1000}}
+          pointerEvents='none'> */}
+
+          {/* <Image
+            source={{uri: 'http://digioh.com/blog/wp-content/uploads/2016/08/teaser.jpg'}}
+            style={{height: 305}}>
+            <View style={{backgroundColor:'#000000'+'26', height:305}}> */}
+              {/* <CalendarPicker
+                textStyle     =   {{color:'white'}}
+                selectedDate  =   {this.state.date}
+                onDateChange  =   {this.onDateChange}
+                screenWidth   =   {Dimensions.get('window').width}
+                selectedBackgroundColor={'#a9a9a9'} /> */}
+
+            {/* </View>
+          </Image> */}
+
+          <View style={{backgroundColor:'white'}}
+            pointerEvents='none'>
             <List>
+
               <ListItem>
                 <Icon name="ios-sunny-outline"/>
                 <View style={{marginLeft:15}}>
@@ -92,8 +147,8 @@ export default class ListPage extends Component {
 
             </List>
           </View>
-        </ScrollView>
-      </View>
+        {/* </ScrollView> */}
+      </ParallaxView>
     );
   }
 }
