109be8322378624c19c16499f4a0f82b2b928986
Minh <hlminh2000@gmail.com>
2017-03-22 22:47:27 -0400

    attempts to fix the keyboard issue

---
+++ components/AuthoringCards/AuthoringCard1.js
@@ -0,0 +1,135 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  Dimensions,
+  TextInput,
+} from 'react-native';
+import {
+  Container,
+  Card
+} from 'native-base';
+import TabNavigator from 'react-navigation';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import ActionButton from 'react-native-action-button';
+import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/Ionicons';
+import NavBar from '../NavBar/NavBar.js';
+
+export default class AuthoringCard1 extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      isFocused: false,
+    }
+  }
+
+  setCardFocused(){
+    this.setState({
+      ...this.state,
+      isFocused: true,
+    });
+  }
+
+  setCardUnfocused(){
+    this.setState({
+      ...this.state,
+      isFocused: false,
+    });
+  }
+
+  render(){
+    return (
+      <Card style={{borderRadius:5}}>
+        <LinearGradient
+          colors={['#FF4E50', '#F9D423']}
+          start={{x: 0.0, y: 0.0}} end={{x: 0.5, y: 1.5}}
+          style={{height:150, borderRadius: 5}}>
+          <LinearGradient
+            colors={
+              (()=>{
+                function pushColor (colorArray, length) {
+                  colorArray.push('rgba(0, 0, 0, 0)')
+                  if(colorArray.length < length) {pushColor(colorArray, length)}
+                  return colorArray;
+                };
+                var output = pushColor([], 4);
+                output.push('rgba(0, 0, 0, 0.4)');
+                return output;
+              })()
+            }
+            style={{
+              height: 1,
+              flex: 1}}/>
+          <Text style={{
+            position:'absolute',
+            left: 10,
+            bottom:40,
+            color:'white',
+            fontSize: 24
+          }}>What I am grateful for...</Text>
+        </LinearGradient>
+
+        <View style={{backgroundColor: 'white', marginTop:-30, marginBottom: 10, paddingTop: 0, paddingBottom: 20}}>
+          <View style={{marginTop: 5, marginBottom: 5, flex: 5, alignItems:'center'}}>
+            <View style={{width:300, paddingTop: 15}}>
+              <Text style={{fontSize: 22, color: '#FF4E50'}}>1</Text>
+              <TextInput
+                // onFocus={this.setCardFocused.bind(this)}
+                // onEndEditing={this.setCardUnfocused.bind(this)}
+                onChangeText={()=>{ console.log("typing!!!"); if(this.props.onInputChange) {this.props.onInputChange()}}}
+                underlineColorAndroid='transparent'
+                autoCapitalize='sentences'
+                style={{
+                  backgroundColor: 'rgba(0, 0, 0, 0.05)',
+                  textAlign: 'center',
+                  fontSize: 18,
+                  flex: 1,
+                  color: 'rgba(0, 0, 0, 0.6)',
+                  height: 40,
+                }}/>
+            </View>
+            <View style={{width:300, paddingTop: 15}}>
+              <Text style={{fontSize: 22, color: '#FF4E50'}}>2</Text>
+              <TextInput
+                // onFocus={this.setCardFocused.bind(this)}
+                // onEndEditing={this.setCardUnfocused.bind(this)}
+                onChangeText={()=>{ console.log("typing!!!"); if(this.props.onInputChange) {this.props.onInputChange()}}}
+                underlineColorAndroid='transparent'
+                autoCapitalize='sentences'
+                style={{
+                  backgroundColor: 'rgba(0, 0, 0, 0.05)',
+                  textAlign: 'center',
+                  fontSize: 18,
+                  flex: 1,
+                  color: 'rgba(0, 0, 0, 0.6)',
+                  height: 40,
+                }}/>
+            </View>
+            <View style={{width:300, paddingTop: 15}}>
+              <Text style={{fontSize: 22, color: '#FF4E50'}}>3</Text>
+              <TextInput
+                // onFocus={this.setCardFocused.bind(this)}
+                // onEndEditing={this.setCardUnfocused.bind(this)}
+                onChangeText={()=>{ console.log("typing!!!"); if(this.props.onInputChange) {this.props.onInputChange()}}}
+                underlineColorAndroid='transparent'
+                autoCapitalize='sentences'
+                style={{
+                  backgroundColor: 'rgba(0, 0, 0, 0.05)',
+                  textAlign: 'center',
+                  fontSize: 18,
+                  flex: 1,
+                  color: 'rgba(0, 0, 0, 0.6)',
+                  height: 40,
+                }}/>
+            </View>
+          </View>
+        </View>
+      </Card>
+    )
+  }
+}
--- components/Scenes/AuthoringPage1.js
+++ components/Scenes/AuthoringPage1.js
@@ -9,20 +9,82 @@ import {
   Image,
   Dimensions,
   TextInput,
+  Keyboard,
+
 } from 'react-native';
 import {
   Container,
   Card
 } from 'native-base';
+import { TabNavigator } from 'react-navigation';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import ActionButton from 'react-native-action-button';
 import LinearGradient from 'react-native-linear-gradient';
 import Icon from 'react-native-vector-icons/Ionicons';
+import NavBar from '../NavBar/NavBar.js';
+import AuthoringCard1 from '../AuthoringCards/AuthoringCard1.js';
+import Carousel from "react-native-carousel-control";
+// import Carousel from "react-native-carousel";
+// import Carousel from 'react-native-snap-carousel';
+
+// import KeyboardEvents from 'react-native-keyboardevents';
+// import {
+//   Emitter as KeyboardEventEmitter
+// } from 'react-native-keyboardevents';
 
 
-import NavBar from '../NavBar/NavBar.js'
+var styles = StyleSheet.create({
+  container: {
+    width: 375,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  },
+});
+
 
 export default class AuthoringPage1 extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      currentPage: 0,
+      isKeyboardOpened: false,
+    }
+  }
+
+  keyboardWillShow() {
+    this.setState({
+      ...this.state,
+      currentPage: this.carousel.state.currentPage,
+      isKeyboardOpened: true
+    });
+  }
+
+  keyboardWillHide() {
+    this.setState({
+      ...this.state,
+      isKeyboardOpened: false
+    });
+  }
+
+  onCardCardContentChange(){
+    console.log("isfbpsdbufsdfoivbfdso");
+    this.setState({
+      ...this.state,
+      currentPage: this.carousel.state.currentPage,
+    });
+    this.forceUpdate();
+  }
+
+  componentDidMount() {
+    // KeyboardEventEmitter.on(KeyboardEvents.KeyboardDidShowEvent, this.updateKeyboardSpace);
+    // KeyboardEventEmitter.on(KeyboardEvents.KeyboardWillHideEvent, this.resetKeyboardSpace);
+    Keyboard.addListener('keyboardDidShow', this.keyboardWillShow.bind(this))
+    Keyboard.addListener('keyboardDidHide', this.keyboardWillHide.bind(this))
+  }
+
   static navigationOptions = {
     title: 'Life',
   }
@@ -37,103 +99,38 @@ export default class AuthoringPage1 extends Component{
     this.props.navigation.navigate('q2');
   }
 
-  renderFirstCard(){
-    return (
-      <Card style={{borderRadius:5}}>
-        <LinearGradient
-          colors={['#FF4E50', '#F9D423']}
-          start={{x: 0.0, y: 0.0}} end={{x: 0.5, y: 1.5}}
-          style={{height:150, borderRadius: 5}}>
-          <LinearGradient
-            colors={
-              (()=>{
-                function pushColor (colorArray, length) {
-                  colorArray.push('rgba(0, 0, 0, 0)')
-                  if(colorArray.length < length) {pushColor(colorArray, length)}
-                  return colorArray;
-                };
-                var output = pushColor([], 4);
-                output.push('rgba(0, 0, 0, 0.4)');
-                return output;
-              })()
-            }
-            style={{
-              height: 1,
-              flex: 1}}/>
-          <Text style={{
-            position:'absolute',
-            left: 10,
-            bottom:40,
-            color:'white',
-            fontSize: 24
-          }}>What I am grateful for...</Text>
-        </LinearGradient>
-
-        <View style={{backgroundColor: 'white', marginTop:-30, marginBottom: 10, paddingTop: 0, paddingBottom: 20}}>
-          <View style={{marginTop: 5, marginBottom: 5, flex: 5, alignItems:'center'}}>
-            <View style={{width:300, paddingTop: 15}}>
-              <Text style={{fontSize: 22, color: '#FF4E50'}}>1</Text>
-              <TextInput
-                underlineColorAndroid='transparent'
-                autoCapitalize='sentences'
-                style={{
-                  backgroundColor: 'rgba(0, 0, 0, 0.1)',
-                  textAlign: 'center',
-                  fontSize: 18,
-                  fkex: 1,
-                  color: 'rgba(0, 0, 0, 0.6)',
-                  height: 40,
-                }}/>
-            </View>
-            <View style={{width:300, paddingTop: 15}}>
-              <Text style={{fontSize: 22, color: '#FF4E50'}}>2</Text>
-              <TextInput
-                underlineColorAndroid='transparent'
-                autoCapitalize='sentences'
-                style={{
-                  backgroundColor: 'rgba(0, 0, 0, 0.1)',
-                  textAlign: 'center',
-                  fontSize: 18,
-                  fkex: 1,
-                  color: 'rgba(0, 0, 0, 0.6)',
-                  height: 40,
-                }}/>
-            </View>
-            <View style={{width:300, paddingTop: 15}}>
-              <Text style={{fontSize: 22, color: '#FF4E50'}}>3</Text>
-              <TextInput
-                underlineColorAndroid='transparent'
-                autoCapitalize='sentences'
-                style={{
-                  backgroundColor: 'rgba(0, 0, 0, 0.1)',
-                  textAlign: 'center',
-                  fontSize: 18,
-                  fkex: 1,
-                  color: 'rgba(0, 0, 0, 0.6)',
-                  height: 40,
-                }}/>
-            </View>
-          </View>
-        </View>
-      </Card>
-    )
-  }
-
   render(){
+
+    // setTimeout(()=>{
+    //   this.setState({
+    //     ...this.state,
+    //     currentPage: this.state.currentPage+1,
+    //   });
+    // }, 1000);
+
+    const cards = [
+      <AuthoringCard1 ref="card1" key={1} onInputChange={this.onCardCardContentChange.bind(this)}/>,
+      <AuthoringCard1 ref="card2" key={2} onInputChange={this.onCardCardContentChange.bind(this)}/>,
+      <AuthoringCard1 ref="card3" key={3} onInputChange={this.onCardCardContentChange.bind(this)}/>,
+      <View key={4}>
+        <Text>GOOD JOB!</Text>
+      </View>
+    ];
+
     return (
       <Container style={{backgroundColor: 'white'}}>
 
         <View style = {{height: 3, width: Dimensions.get('window').width/3, backgroundColor: '#FF4E50'}}></View>
 
-        <ScrollView style={{alignItems:'center'}}>
+        <ScrollView contentContainerStyle={{alignItems:'center'}}>
 
           <View style={{
               flex: 1,
               flexDirection:'row',
               paddingTop: 20,
               paddingBottom: 10,
-              paddingLeft: 10,
-              paddingRight: 10}}>
+              paddingLeft: 20,
+              paddingRight: 20}}>
             <Image
               source={{uri: 'https://images-na.ssl-images-amazon.com/images/I/81VStYnDGrL.jpg'}}
               style={{height: 60, width: 60, borderRadius:50}}
@@ -148,11 +145,70 @@ export default class AuthoringPage1 extends Component{
             </View>
           </View>
 
-          <View style={{flex:5, width: 340}}>
-            {this.renderFirstCard.bind(this)()}
-          </View>
+          {/* <ScrollView
+            horizontal
+            scrollEnabled={ !this.state.isKeyboardOpened }
+            >
+            {cards} */}
+            <Carousel
+              swipeThreshold={0.05}
+              pageWidth={Dimensions.get('window').width - 35}
+              currentPage={this.state.currentPage}
+              onPageChange={()=>{console.log("changing!!!");}}
+              ref={(carousel) => { this.carousel = carousel; }}
+              // currentPage={
+              //   // cards.indexOf(
+              //     (()=>{
+              //       const focusedCard = cards.filter((card)=>card.state.isFocused)
+              //       return focusedCard ? cards.indexOf(ocusedCard) : 0;
+              //     })()
+              //   // )
+              // }
+              sneak={0.5}>
+              { cards }
+            </Carousel>
+          {/* </ScrollView> */}
+
+          {/* <Carousel width={375}>
+            <View style={styles.container}>
+              <Text>Page 1</Text>
+            </View>
+            <View style={styles.container}>
+              <Text>Page 2</Text>
+            </View>
+            <View style={styles.container}>
+              <Text>Page 3</Text>
+            </View>
+          </Carousel> */}
+
+          {/* <Carousel
+            ref={'carousel'}
+            sliderWidth={Dimensions.get('window').width - 35}
+            itemWidth={Dimensions.get('window').width - 35} >
+            <AuthoringCard1/>
+            <AuthoringCard1/>
+            <AuthoringCard1/>
+            <View>
+              <Text>GOOD JOB!</Text>
+            </View>
+          </Carousel> */}
+
+          {/* <TextInput
+            // onFocus={this.setCardFocused.bind(this)}
+            // onEndEditing={this.setCardUnfocused.bind(this)}
+            underlineColorAndroid='transparent'
+            autoCapitalize='sentences'
+            style={{
+              backgroundColor: 'rgba(0, 0, 0, 0.05)',
+              textAlign: 'center',
+              fontSize: 18,
+              flex: 1,
+              color: 'rgba(0, 0, 0, 0.6)',
+              height: 40,
+            }}/> */}
 
         </ScrollView>
+
 
         <ActionButton
           onPress={() => { this.goToNextPage.bind(this) }}
