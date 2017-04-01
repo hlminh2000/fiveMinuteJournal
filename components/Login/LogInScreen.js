1402ab77990fd82ac1b882b4babba0baf8c8eabb
Minh <hlminh2000@gmail.com>
2017-04-01 08:43:46 -0400

    gets loading circle working on login

--- components/Login/LogInScreen.js
+++ components/Login/LogInScreen.js
@@ -13,8 +13,8 @@ import {
   Button,
 } from 'native-base';
 import LinearGradient from 'react-native-linear-gradient';
-import FirebaseApp from '../../firebase/Firebase.js';
-
+import Firebase from '../../firebase/Firebase.js';
+import Spinner from 'react-native-spinkit';
 
 
 export default class LogInScreen extends Component {
@@ -24,21 +24,31 @@ export default class LogInScreen extends Component {
     this.state = {
       email:  null,
       password: null,
+      isLoggingIn: false,
     }
   }
 
   signInWithEmailAndPassword(){
-    FirebaseApp.auth()
+    if(this.state.email && this.state.password){
+      this.setState({
+        ...this.state,
+        isLoggingIn: true,
+      })
+      Firebase.auth()
       .signInWithEmailAndPassword(this.state.email, this.state.password)
       .then(() => {
-        console.log(FirebaseApp.auth().currentUser);
         if(this.props.onLoginComplete){
           this.props.onLoginComplete();
         }
       })
       .catch((err) => {
+        this.setState({
+          ...this.state,
+          isLoggingIn: false,
+        })
         console.log(err);
       })
+    }
   }
 
   render() {
@@ -54,67 +64,86 @@ export default class LogInScreen extends Component {
             colors={['rgba(67,198,172, 0.7)', 'rgba(248,255,174, 0.7)']}
             start={{x: 0.0, y: 0.0}} end={{x: 0.5, y: 1.5}}
             style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <View style={{marginTop: 20}}>
-              <TextInput
-                onChangeText={(text)=>{ this.state.email = text}}
-                underlineColorAndroid='transparent'
-                autoCapitalize='sentences'
-                placeholder="email"
-                // value={this.state.email}
-                placeholderTextColor='rgba(255, 255, 255, 0.3)'
-                style={{
-                  backgroundColor: 'rgba(0, 0, 0, 0.07)',
-                  textAlign: 'center',
-                  color: 'white',
-                  fontSize: 18,
-                  height: 40,
-                  width: windowDimention.width - 50,
-                }}/>
-            </View>
-            <View style={{marginTop: 20}}>
-              <TextInput
-                onChangeText={(text)=>{ this.state.password = text}}
-                underlineColorAndroid='transparent'
-                autoCapitalize='sentences'
-                placeholder="password"
-                // value={this.state.password}
-                placeholderTextColor='rgba(255, 255, 255, 0.3)'
-                style={{
-                  backgroundColor: 'rgba(0, 0, 0, 0.07)',
-                  textAlign: 'center',
-                  color: 'white',
-                  fontSize: 18,
-                  height: 40,
-                  width: windowDimention.width - 50,
-                }}/>
-            </View>
-            <View style={{marginTop: 20}}>
-              <Button
-                style={{width: windowDimention.width-150, borderRadius: 5, justifyContent:'center', flexDirection: 'column', paddingTop: 0, paddingBottom: 0}}>
-                <TouchableNativeFeedback
-                  onPress={ this.signInWithEmailAndPassword.bind(this) }
-                  background={TouchableNativeFeedback.SelectableBackground()}
-                  useForeground={true}>
-                  <LinearGradient
-                    colors={['#FF4E50', '#F9D423']}
-                    start={{x: 0.0, y: 0.0}} end={{x: 0.5, y: 1.5}}
-                    style={{
-                      borderRadius: 5,
-                      width: windowDimention.width-150,
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      flex:1,
-                    }}>
-                      <Text style={{fontSize: 16, color: 'white'}}>Log in</Text>
-                  </LinearGradient>
-                </TouchableNativeFeedback>
-              </Button>
-            </View>
-            <View style={{marginTop: 20}}>
-              <Button style={{width: windowDimention.width-150, borderRadius: 5, justifyContent: 'center', backgroundColor: 'lightgrey'}}>
-                <Text style={{fontSize: 16, color: 'grey'}}>Sign up</Text>
-              </Button>
-            </View>
+            {
+              (()=>{ if(!this.state.isLoggingIn){
+                  return (
+                    <View style={{alignItems:'center'}}>
+                      <View style={{marginTop: 20}}>
+                        <TextInput
+                          onChangeText={(text)=>{ this.setState({ ...this.state, email: text }) }}
+                          underlineColorAndroid='transparent'
+                          autoCapitalize='sentences'
+                          placeholder="email"
+                          value={this.state.email}
+                          placeholderTextColor='rgba(255, 255, 255, 0.3)'
+                          style={{
+                            backgroundColor: 'rgba(0, 0, 0, 0.07)',
+                            textAlign: 'center',
+                            color: 'white',
+                            fontSize: 18,
+                            height: 40,
+                            width: windowDimention.width - 50,
+                          }}/>
+                      </View>
+                      <View style={{marginTop: 20}}>
+                        <TextInput
+                          onChangeText={(text)=>{ this.setState({ ...this.state, password: text }) }}
+                          underlineColorAndroid='transparent'
+                          autoCapitalize='sentences'
+                          placeholder="password"
+                          value={this.state.password}
+                          placeholderTextColor='rgba(255, 255, 255, 0.3)'
+                          style={{
+                            backgroundColor: 'rgba(0, 0, 0, 0.07)',
+                            textAlign: 'center',
+                            color: 'white',
+                            fontSize: 18,
+                            height: 40,
+                            width: windowDimention.width - 50,
+                          }}/>
+                      </View>
+                      <View style={{marginTop: 20}}>
+                        <Button
+                          style={{
+                            width: windowDimention.width-150,
+                            borderRadius: 5,
+                            justifyContent:'center',
+                            flexDirection: 'column',
+                            paddingTop: 0,
+                            paddingBottom: 0}}>
+                        <TouchableNativeFeedback
+                          onPress={ this.signInWithEmailAndPassword.bind(this) }
+                          background={TouchableNativeFeedback.SelectableBackground()}
+                          useForeground={true}>
+                          <LinearGradient
+                            colors={['#FF4E50', '#F9D423']}
+                            start={{x: 0.0, y: 0.0}} end={{x: 0.5, y: 1.5}}
+                            style={{
+                              borderRadius: 5,
+                              width: windowDimention.width-150,
+                              justifyContent: 'center',
+                              alignItems: 'center',
+                              flex:1,
+                            }}>
+                            <Text style={{fontSize: 16, color: 'white'}}>Log in</Text>
+                          </LinearGradient>
+                        </TouchableNativeFeedback>
+                      </Button>
+                    </View>
+                    <View style={{marginTop: 20}}>
+                      <Button style={{
+                          width: windowDimention.width-150,
+                          borderRadius: 5,
+                          justifyContent: 'center',
+                          backgroundColor: 'lightgrey'}}>
+                        <Text style={{fontSize: 16, color: 'grey'}}>Sign up</Text>
+                      </Button>
+                    </View>
+                  </View>
+                )
+              }})()
+            }
+            <Spinner isVisible={this.state.isLoggingIn} color={"white"} size={windowDimention.width / 3} type={'Bounce'}/>
           </LinearGradient>
         </Image>
       </Container>
--- firebase/Firebase.js
+++ firebase/Firebase.js
@@ -1,8 +1,8 @@
 import API_ACCOUNTS from '../configs/API_ACCOUNTS.json';
 import * as firebase from 'firebase';
 
-const FirebaseApp = firebase.initializeApp(API_ACCOUNTS.FIREBASE);
+const Firebase = firebase.initializeApp(API_ACCOUNTS.FIREBASE);
 var provider = new firebase.auth.FacebookAuthProvider();
 
 
-export default FirebaseApp;
+export default Firebase;
